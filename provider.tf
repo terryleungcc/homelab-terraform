a9dd6f6405b68dc4d8a251c563de4f5c9d57a182
Terry Leung <59078445+terrylcc@users.noreply.github.com>
2023-09-11 11:42:58 +0800

    feat: initialize terraform providers

---
+++ provider.tf
@@ -0,0 +1,18 @@
+provider "kubernetes" {
+  host = "https://192.168.1.2:6443"
+
+  client_certificate     = file("~/.kube/client-cert.pem")
+  client_key             = file("~/.kube/client-key.pem")
+  cluster_ca_certificate = file("~/.kube/cluster-ca-cert.pem")
+}
+
+provider "helm" {
+  kubernetes {
+    host = "https://192.168.1.2:6443"
+
+    client_certificate     = file("~/.kube/client-cert.pem")
+    client_key             = file("~/.kube/client-key.pem")
+    cluster_ca_certificate = file("~/.kube/cluster-ca-cert.pem")
+  }
+}
+
